ffbd4fccf82e0105545c87f7906c87adca746792
Luchitha7 <185213284+Luchitha7@users.noreply.github.com>
2026-04-30 17:00:51 +0530

    Defer IS algorithms and keep GV Day 1 scope

--- Assets/Scripts/Algorithms/PriorityQueue.cs
+++ Assets/Scripts/Algorithms/PriorityQueue.cs
@@ -1,86 +1,8 @@
-using System;
-using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
-/// Generic min-heap priority queue used by pathfinding algorithms.
+/// Provides queue ordering support for pathfinding algorithm processing.
 /// </summary>
-public class PriorityQueue<T>
+public class PriorityQueue : MonoBehaviour
 {
-    private readonly List<(T Item, float Priority)> heap = new List<(T Item, float Priority)>();
-
-    public int Count => heap.Count;
-
-    public void Enqueue(T item, float priority)
-    {
-        heap.Add((item, priority));
-        HeapifyUp(heap.Count - 1);
-    }
-
-    public T Dequeue()
-    {
-        if (heap.Count == 0)
-        {
-            throw new InvalidOperationException("Cannot dequeue from an empty priority queue.");
-        }
-
-        T minItem = heap[0].Item;
-        int lastIndex = heap.Count - 1;
-        heap[0] = heap[lastIndex];
-        heap.RemoveAt(lastIndex);
-
-        if (heap.Count > 0)
-        {
-            HeapifyDown(0);
-        }
-
-        return minItem;
-    }
-
-    private void HeapifyUp(int index)
-    {
-        while (index > 0)
-        {
-            int parentIndex = (index - 1) / 2;
-            if (heap[parentIndex].Priority <= heap[index].Priority)
-            {
-                break;
-            }
-
-            Swap(parentIndex, index);
-            index = parentIndex;
-        }
-    }
-
-    private void HeapifyDown(int index)
-    {
-        while (true)
-        {
-            int leftChild = (2 * index) + 1;
-            int rightChild = (2 * index) + 2;
-            int smallest = index;
-
-            if (leftChild < heap.Count && heap[leftChild].Priority < heap[smallest].Priority)
-            {
-                smallest = leftChild;
-            }
-
-            if (rightChild < heap.Count && heap[rightChild].Priority < heap[smallest].Priority)
-            {
-                smallest = rightChild;
-            }
-
-            if (smallest == index)
-            {
-                break;
-            }
-
-            Swap(index, smallest);
-            index = smallest;
-        }
-    }
-
-    private void Swap(int a, int b)
-    {
-        (heap[a], heap[b]) = (heap[b], heap[a]);
-    }
 }
